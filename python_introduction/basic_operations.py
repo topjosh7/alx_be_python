9d44584dac15971c619ba082daea17890f9bd9f6
Joshua Ajayi <topjosh7@gmail.com>
2025-11-06 01:30:59 +0000

    Add basic_operations.py for arithmetic exercise

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,5 @@
+number1 = 10
+number2 = 5
+print(f"Sum of {number1} and {number2} is {number1 + number2}.")
+print(f"Difference of {number1} and {number2} is {number1 - number2}.")
+print(f"Product of {number1} and {number2} is {number1 * number2}.")
