4581b9b6f63cceb9cffed763e7657eda1f07b67c
Joshua Ajayi <topjosh7@gmail.com>
2025-11-30 00:04:13 +0100

    Completed unit tests for SimpleCalculator class

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -6,23 +6,23 @@ class TestSimpleCalculator(unittest.TestCase):
     def setUp(self):
         self.calc = SimpleCalculator()
 
-    def test_add(self):
+    def test_addition(self):
         self.assertEqual(self.calc.add(3, 5), 8)
         self.assertEqual(self.calc.add(-2, 7), 5)
         self.assertEqual(self.calc.add(0, 0), 0)
 
-    def test_subtract(self):
+    def test_subtraction(self):
         self.assertEqual(self.calc.subtract(10, 4), 6)
         self.assertEqual(self.calc.subtract(5, 10), -5)
         self.assertEqual(self.calc.subtract(0, 0), 0)
 
-    def test_multiply(self):
+    def test_multiplication(self):
         self.assertEqual(self.calc.multiply(3, 4), 12)
         self.assertEqual(self.calc.multiply(-2, 6), -12)
         self.assertEqual(self.calc.multiply(0, 100), 0)
 
-    def test_divide(self):
-        # Only test valid division (denominator != 0)
+    def test_division(self):
+        # Only test valid division
         self.assertEqual(self.calc.divide(10, 2), 5)
         self.assertEqual(self.calc.divide(9, -3), -3)
         self.assertEqual(self.calc.divide(7, 7), 1)
